344f601ade0e6cac45b2695af4dbc517601dc44a
Ricardo Geraldes <rgeraldes24@gmail.com>
2020-03-29 21:01:42 +0100

    Added trigger to limit the number of user accounts

--- scripts/migrations/000001_create_user_keys_table.up.sql
+++ scripts/migrations/000001_create_user_keys_table.up.sql
@@ -8,3 +8,16 @@ CREATE TABLE IF NOT EXISTS user_keys(
     updated_at timestamp NULL DEFAULT NULL,
     PRIMARY KEY(id)
 ) ENGINE = INNODB;
+
+CREATE TRIGGER max_keys
+BEFORE INSERT ON user_keys
+FOR EACH ROW
+BEGIN
+    DECLARE cnt INT;
+
+    SELECT count(*) INTO cnt FROM user_keys;
+
+    IF cnt = 3 THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'You can store only 3 records.';
+    END IF;
+END;
